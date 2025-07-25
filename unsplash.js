0e051bee94f5b717ee98d800c8f07355b644513d
JUBAYER AHMED - {inx} <gmastatine@gmail.com>
2025-07-26 00:44:05 +0600

    Update unsplash.js

--- unsplash.js
+++ unsplash.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const path = require("path");
 const fs = require("fs");
 
-const jubayer = "https://raw.githubusercontent.com/VincentXJubayer/JUBAYE4/main/baseApiUrl.json";
+const jubayer = "https://jubayer-unsplash-apis.vercel.app";
 
 module.exports = {
   config: {
